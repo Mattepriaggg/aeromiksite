772fa905ac2bab19abfee9fe0111aca138886f36
Mattepriagggg <mattepriaggg@gmail.com>
2022-11-01 20:20:09 +0100

    iii

--- animation.js
+++ animation.js
@@ -1,38 +1,17 @@
-// STORIA
-let item = document.querySelector('.toggle');
-let panel = document.querySelector('.panel');
-let sec1 = document.querySelector('.sec1');
-
-item.addEventListener("click", function() {
-    panel.classList.toggle('active');
-
-    sec1.classList.toggle('sec1_j');
-});
-
-
-// MAIL ANIMATION
-let submit_Button = document.querySelector('.submit_Button');
-
-submit_Button.addEventListener("click", function() {
-    submit_Button.classList.toggle('borderyes');
-});
-
-
 
 
 // MODAL FOTO
-function togglePopup1(){
-  document.getElementById("popup-1").classList.toggle("activePOP")
-}
 
-function togglePopup2(){
-  document.getElementById("popup-2").classList.toggle("activePOP")
-}
 
-function togglePopup3(){
-  document.getElementById("popup-3").classList.toggle("activePOP")
-}
+//--SCROLL IMG--
+let int = document.querySelector('.int');
+let sub = document.querySelector('.sub');
+let sub2 = document.querySelector('.sub2');
 
-function togglePopup4(){
-  document.getElementById("popup-4").classList.toggle("activePOP")
-}
+int.addEventListener("click", function() {
+    
+    sub.classList.toggle('scrollin');
+    sub2.classList.toggle('sc2');
+});
+
+
